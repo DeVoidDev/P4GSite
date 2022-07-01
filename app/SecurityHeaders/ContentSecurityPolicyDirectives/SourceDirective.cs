38f88c6566d613581402fd105fe789f870831e80
DeVoidDev <67163402+DeVoidDev@users.noreply.github.com>
2022-07-01 13:46:37 +0200

    Add Missing SourceDirective Class

---
+++ app/SecurityHeaders/ContentSecurityPolicyDirectives/SourceDirective.cs
@@ -0,0 +1,55 @@
+﻿namespace P4GSite.SecurityHeaders.ContentSecurityPolicyDirectives;
+
+public abstract class SourceDirective : IDirective
+{
+    private readonly List<string> _list = new();
+
+    public void AddHost(string host, int port, string? scheme = null)
+    {
+        _list.Add($"{(scheme != null ? $"{scheme}://" : string.Empty)}{host}:{port}");
+    }
+
+    public void AddScheme(string scheme)
+    {
+        _list.Add($"{scheme}:");
+    }
+
+    public void AddSelf()
+    {
+        _list.Add("'self'");
+    }
+
+    [Obsolete("Eval is unsafe.")]
+    public void AddEval()
+    {
+        _list.Add("'unsafe-eval'");
+    }
+
+    [Obsolete("Hashed scripts are unsafe.")]
+    public void AddHashes()
+    {
+        _list.Add("'unsafe-hashes'");
+    }
+
+    [Obsolete("Inline scripts are unsafe.")]
+    public void AddInline()
+    {
+        _list.Add("'unsafe-inline'");
+    }
+
+    public void AddNone()
+    {
+        _list.Add("'none'");
+    }
+
+    public void AddStrictDynamic()
+    {
+        _list.Add("'strict-dynamic'");
+    }
+
+
+    public override string ToString()
+    {
+        return string.Join(' ', _list);
+    }
+}
